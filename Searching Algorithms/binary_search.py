76bfaa4a29c20eca9a1d073cd9850fcae6f81475
Alaa Samy <64163691+alaa-samy@users.noreply.github.com>
2022-11-24 03:52:57 +0200

    Update binary_search.py

--- Searching Algorithms/binary_search.py
+++ Searching Algorithms/binary_search.py
@@ -1,19 +1,27 @@
 # Binary Search Algorithm
 def binary_search(array , n):
-  for i in range(0 , len(array)):
-    if (array[i] == n):
-      return i
-  return -1
+    low = 0
+    high = len(array)-1
+    
+    while (low <= high):
+        mid = low + (high - low)//2
+        if (array[mid] == n):
+            return mid
+        elif (array[mid] < n):
+            low = mid + 1
+        else:
+            high = mid - 1
+    return -1
 
 
-array = [2 , 5 , 1 , 7]
-n = 4
-result = linear_search(array , n)
+array = [2 , 5 , 4 , 7 , 6]
+n = 7
+result = binary_search(array , n)
 
 if result == -1:
   print("Element not found")
 else:
   print("Element is found at index ", result)
 
-# Time Complexity ---> O(n)
+# Time Complexity ---> O(logn)
 # Space Complexity ---> O(1)
